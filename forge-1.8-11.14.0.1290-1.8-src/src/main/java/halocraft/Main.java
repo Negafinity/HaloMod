e3dbf31cb1e125f17dcd3e4675bbd820f6683c73
Hassan Syyid <hassansyyid@gmail.com>
2015-01-20 17:12:30 -0500

    Created Elite
    CREATED THE ELITE MOB :D

--- forge-1.8-11.14.0.1290-1.8-src/src/main/java/halocraft/Main.java
+++ forge-1.8-11.14.0.1290-1.8-src/src/main/java/halocraft/Main.java
@@ -1,19 +1,25 @@
 package halocraft;
 
+import net.minecraft.world.World;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
 import net.minecraft.item.ItemSword;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.common.registry.LanguageRegistry;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EnumCreatureType;
 
 @Mod(modid="halocraft", version="pre1.0")
 public class Main{
@@ -21,6 +27,7 @@ public class Main{
 	public static String VERSION = "1";
 	public static ItemSword swordEnergySword;
 	public static Block forerunnerBlock;
+	public static Entity mobElite;
 	@EventHandler
 	public void init(FMLInitializationEvent e){
 		swordEnergySword = new swordEnergySword(ToolMaterial.EMERALD);
@@ -29,6 +36,11 @@ public class Main{
 		GameRegistry.registerBlock(forerunnerBlock, "forerunnerBlock");
 		ModelResourceLocation res = new ModelResourceLocation("halocraft:energySword", "inventory");
 		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(swordEnergySword, 0, res);
+		mobElite = new EntityElite(null);
+		int randomID = EntityRegistry.findGlobalUniqueEntityId();
+		EntityRegistry.registerGlobalEntityID(EntityElite.class, "Elite", randomID, 230, 78);
+		EntityRegistry.addSpawn(EntityElite.class, 5, 1, 2, EnumCreatureType.MONSTER);
+		RenderingRegistry.registerEntityRenderingHandler(EntityElite.class, new RenderEliteEntity(Minecraft.getMinecraft().getRenderManager(), new ModelBiped(), 0));
 	}
 	public static CreativeTabs haloWeapons = new CreativeTabs("Halo Weapons"){
 	public Item getTabIconItem(){
