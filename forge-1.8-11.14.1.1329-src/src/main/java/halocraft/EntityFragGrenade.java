de0a0ea6ecff90cb3bd531761f9b12c6fd90e426
Hassan Syyid <hassansyyid@gmail.com>
2015-03-01 17:59:31 -0500

    HaloCraft 2.0 v 1.1 Started!
    - Work beginning on Mongoose Model (So it looks more like a mongoose and
    less like a boat)
    
    - Halo HUD Created! :smile:
    - HUD HAS WORKING HEALTH BAR
    - HUD HAS NON-WORKING MAP
    - HUD IS CENTERED
    - HUD DOES NOT ALWAYS TAKE UP FULL SCREEN (ESPECIALLY ON LARGER SCREEN
    SIZES THAN 854 x 480)

---
+++ forge-1.8-11.14.1.1329-src/src/main/java/halocraft/EntityFragGrenade.java
@@ -0,0 +1,32 @@
+package halocraft;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
+
+
+public class EntityFragGrenade extends EntityThrowable{
+
+	public EntityFragGrenade(World worldIn) {
+		super(worldIn);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EntityFragGrenade(World worldIn, EntityLivingBase entityIn){
+		super(worldIn, entityIn);
+	}
+
+	@Override
+	protected void onImpact(MovingObjectPosition p_70184_1_) {
+		if(!this.worldObj.isRemote){
+			this.setDead();
+			if(!this.worldObj.isRemote){
+				this.worldObj.createExplosion(this, this.posX, this.posY, this.posZ, 2.0F, true);
+				this.setDead();
+			}
+		}
+		
+	}
+}
