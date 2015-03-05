1bba72df11b5744ef5a9165265f270611869b723
Hassan Syyid <hassansyyid@gmail.com>
2015-03-05 13:59:09 -0500

    Carbine Rifle
    - Added Carbine Rifle
    - Added Plasma
    - Updated Forge

---
+++ forge-1.8-11.14.1.1336-src/src/main/java/halocraft/ItemCarbineRifle.java
@@ -0,0 +1,26 @@
+package halocraft;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class ItemCarbineRifle extends Item {
+	public ItemCarbineRifle(){
+	setCreativeTab(CreativeTabs.tabCombat);
+	setMaxStackSize(1);
+	setUnlocalizedName("CarbineRifle");
+	}
+	public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn){
+	     if(playerIn.capabilities.isCreativeMode||playerIn.inventory.consumeInventoryItem(halocraft.Main.ammoPlasma)){
+	         worldIn.playSoundAtEntity(playerIn, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+	         if (!worldIn.isRemote)
+	         {
+	             worldIn.spawnEntityInWorld(new EntityGreenPlasma(worldIn, playerIn));
+	         }
+	         return itemStackIn;
+	   }
+	   return itemStackIn;
+	}
+}
---
+++ forge-1.8-11.14.1.1336-src/src/main/java/halocraft/ammoPlasma.java
@@ -0,0 +1,11 @@
+package halocraft;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ammoPlasma extends Item {
+	public ammoPlasma(){
+		setCreativeTab(CreativeTabs.tabCombat);
+		setUnlocalizedName("ammoPlasma");
+	}
+}
