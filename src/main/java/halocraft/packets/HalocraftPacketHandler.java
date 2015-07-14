6996143daa59d0c0ddc36c6a04e2666760f372e3
HassanS6000 <hassansyyid@gmail.com>
2015-07-14 16:21:37 -0400

    GitHub Not Cooperating.
    GitHub Not Cooperating.

--- src/main/java/halocraft/packets/HalocraftPacketHandler.java
+++
@@ -1,9 +0,0 @@
-package halocraft.packets;
-
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-
-public class HalocraftPacketHandler {
-public static final SimpleNetworkWrapper INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel("halocraft");
-
-}
