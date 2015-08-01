f73afa77a456c267589b852273f3d8a565b337b5
HassanS6000 <hassansyyid@gmail.com>
2015-08-01 17:09:10 -0400

    Scorpion now works correctly on Servers
    Scorpion now works correctly on Servers

--- src/main/java/halocraft/handlers/VehicleEventHandler.java
+++ src/main/java/halocraft/handlers/VehicleEventHandler.java
@@ -2,7 +2,6 @@ package halocraft.handlers;
 
 import halocraft.entities.EntityScorpion;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -13,21 +12,9 @@ public class VehicleEventHandler {
 	@SubscribeEvent
 	public void onRenderPlayerEvent(RenderPlayerEvent.Pre event)
 	{
-		if(mc.thePlayer.isRiding() && mc.thePlayer.ridingEntity instanceof EntityScorpion)
+		if(event.entityPlayer.isRiding() && event.entityPlayer.ridingEntity instanceof EntityScorpion)
 		{
 			event.setCanceled(true);
-		}
-	}
-
-	@SubscribeEvent
-	public void onRenderGameOverlayEvent(RenderGameOverlayEvent.Pre event)
-	{
-		if(mc.thePlayer.isRiding() && mc.thePlayer.ridingEntity instanceof EntityScorpion)
-		{
-			if(event.type == event.type.HOTBAR)
-			{
-				event.setCanceled(true);
-			}
 		}
 	}
 }
