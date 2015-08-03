cc3ed814435be2ba08bc1fefb1276bbe17e20a2d
HassanS6000 <hassansyyid@gmail.com>
2015-08-03 16:15:54 -0400

    Added Pulse Grenade
    Added Pulse Grenade

---
+++ src/main/java/halocraft/items/PulseGrenade.java
@@ -0,0 +1,35 @@
+package halocraft.items;
+
+import halocraft.entities.EntityPulseGrenade;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class PulseGrenade extends Item
+{
+	public static final PulseGrenade instance = new PulseGrenade();
+	public static final String name = "PulseGrenade";
+
+	public PulseGrenade()
+	{
+		setUnlocalizedName("pulseGrenade");
+		setCreativeTab(halocraft.Main.haloCreativeTab);
+	}
+
+	public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn)
+	{
+		if (!playerIn.capabilities.isCreativeMode)
+		{
+			--itemStackIn.stackSize;
+		}
+		
+		worldIn.playSoundAtEntity(playerIn, "random.fizz", 0.7F, 0.8F);
+
+		if (!worldIn.isRemote)
+		{
+			worldIn.spawnEntityInWorld(new EntityPulseGrenade(worldIn, playerIn));
+		}
+		return itemStackIn;
+	}
+}
