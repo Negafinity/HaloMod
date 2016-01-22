c378ef32bbd1ce7ccac5891c46a4df4be8c7ac51
Hassan Syyid <hassansyyid@gmail.com>
2016-01-22 14:41:06 -0500

    Release

--- src/main/java/halocraft/items/firearms/ItemFirearm.java
+++ src/main/java/halocraft/items/firearms/ItemFirearm.java
@@ -10,7 +10,7 @@ import net.minecraft.world.World;
 import java.util.Random;
 
 public class ItemFirearm extends Item
-{	
+{
 	public Item ammo;
 	public int damage;
 	public int clipRounds;
@@ -28,6 +28,7 @@ public class ItemFirearm extends Item
 		if (playerIn.capabilities.isCreativeMode || this.canDamageAmmo(worldIn, playerIn))
 		{
 			worldIn.playSoundAtEntity(playerIn, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+			
 			if (!worldIn.isRemote)
 			{
 				EntityBullet bullet = new EntityBullet(worldIn, playerIn);
@@ -35,9 +36,10 @@ public class ItemFirearm extends Item
 				worldIn.spawnEntityInWorld(bullet);
 				itemStackIn.damageItem(1, playerIn);
 			}
+			
 			return itemStackIn;
 		}
-		
+
 		return itemStackIn;
 	}
 
@@ -62,6 +64,7 @@ public class ItemFirearm extends Item
 				}
 			}
 		}
+		
 		return false;
 	}
 }
